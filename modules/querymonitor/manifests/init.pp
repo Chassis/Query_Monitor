343d14d52eac8b01c231a78c346ffd38c19e6259
Bronson Quick <bronson@sennza.com.au>
2016-05-02 22:07:24 +1000

    Ensure symlink is created and caps are set

--- modules/querymonitor/manifests/init.pp
+++ modules/querymonitor/manifests/init.pp
@@ -1,16 +1,29 @@
 class querymonitor (
-  $path = "/vagrant/extensions/themereview"
+  $path = "/vagrant/extensions/querymonitor"
 ) {
-  file { "/vagrant/content":
-    ensure => "directory",
+  if ! ( File['/vagrant/content'] ) {
+    file { '/vagrant/content':
+      ensure => "directory",
+    }
   }
-  file { "/vagrant/content/plugins":
-    ensure => "directory",
+  if ! ( File['/vagrant/content/plugins'] ) {
+    file { '/vagrant/content/plugins':
+      ensure => "directory",
+    }
   }
   wp::plugin { 'query-monitor':
     ensure   => enabled,
     location => '/vagrant/wp',
     require  => Class['wp'],
   }
-
+  file { '/vagrant/content/db.php':
+    ensure => 'link',
+    target => '/vagrant/content/plugins/query-monitor/wp-content/db.php',
+    require => Wp::Plugin['query-monitor'],
+  }
+  exec { "/usr/bin/wp cap add 'administrator' 'view_query_monitor'":
+    user => 'www-data',
+    require  => [ Class['wp'], Wp::Plugin['query-monitor'] ],
+    cwd => '/vagrant/wp',
+  }
 }
