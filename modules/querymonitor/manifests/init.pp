cfaf18258c87075b25123c3597059a0567c1ebb6
Stephen Edgar <stephen@netweb.com.au>
2017-03-21 20:16:18 +1100

    Fix indentation alignment

--- modules/querymonitor/manifests/init.pp
+++ modules/querymonitor/manifests/init.pp
@@ -18,13 +18,13 @@ class querymonitor (
     require  => Class['wp'],
   }
   file { '/vagrant/content/db.php':
-    ensure => 'link',
-    target => '/vagrant/content/plugins/query-monitor/wp-content/db.php',
+    ensure  => 'link',
+    target  => '/vagrant/content/plugins/query-monitor/wp-content/db.php',
     require => Wp::Plugin['query-monitor'],
   }
   exec { "/usr/bin/wp cap add 'administrator' 'view_query_monitor'":
-    user => 'www-data',
-    require  => [ Class['wp'], Wp::Plugin['query-monitor'] ],
-    cwd => '/vagrant/wp',
+    user    => 'www-data',
+    require => [ Class['wp'], Wp::Plugin['query-monitor'] ],
+    cwd     => '/vagrant/wp',
   }
 }
